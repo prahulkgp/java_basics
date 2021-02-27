fafe5af0760262cc6ca05d627c947b876694f9a2
prahulkgp <prahulkgp@gmail.com>
2021-02-27 19:10:39 +0530

    Daily coding problem #1.
    Bonus is to see if this can be done in one "pass", which I think means to read through the input list array only once.

---
+++ src/main/java/DCP1.java
@@ -0,0 +1,39 @@
+/*
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ */
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class DCP1 {
+    public static void main (String[] args) {
+        System.out.println("How many numbers do you want to input for checking?");
+        Scanner read = new Scanner(System.in);
+        int size = read.nextInt();
+        int[] input = new int[size];
+        System.out.println("Enter the list of numbers.");
+        for (int i = 0; i < size; i++) {
+            input[i] = read.nextInt();
+        }
+        System.out.println("What's the sum to check the list against?");
+        int sum = read.nextInt();
+
+        if (sumCheck(input, sum) == 1) {
+            System.out.println("Such pair exists.");
+        }
+        else {
+            System.out.println("Such pair doesn't exist.");
+        }
+    }
+    static int sumCheck(int[] input, int sum) {
+        int result = 0;
+        for (int i = 0; i < (input.length - 1); i++) {
+            int diff = sum - input[i];
+            for (int j = i + 1; j < input.length; j++) {
+                if (diff != input[j]) ;
+                else return 1;
+            }
+        }
+        return result;
+    }
+}
--- src/main/java/Spiral.java
+++ src/main/java/Spiral.java
@@ -6,15 +6,15 @@ public class Spiral {
     public ArrayList<Integer> spiralOrder(final List<ArrayList<Integer>> A) {
         int I, J, K, L;
         ArrayList<Integer> Output = new ArrayList<>();
-        for (I = 0; I <= 2; I++) {
-            for (J = 0; J <= 2; J++) {
-                Output.add(A.get(I).get(J));
+        for (I = 0, J = 0, K = 0, L = A.size(); (I < 3 & J < 3 & K < 3 & L > 0); I++, J++, K++, L--) {
+            while (J < (A.get(0).size() - J)) {
+                Output.add(A.get(I).get(J)); J++;
             }
-            for (K = 0; K <= 2; K++) {
-                Output.add(A.get(J).get(K));
+            while (K < (A.get(0).size() - 1)) {
+                Output.add(A.get(J).get(K)); K++;
             }
-            for (L = 1; L >= 0; L--) {
-                Output.add(A.get(K).get(L));
+            while (L > (A.size() - I)) {
+                Output.add(A.get(K).get(L)); L--;
             }
         }
         return Output;
